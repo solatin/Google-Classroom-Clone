a56b894f37a41957cb020ab6b184f20ccb0574f0
ĐẠT TRẦN TUẤN <18120313@student.hcmus.edu.vn>
2021-11-22 17:08:13 +0700

    Create send Invitation

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+var nodemailer = require('nodemailer');
 const cors = require('cors');
 require('dotenv').config();
 
 const Class = require('./models/class.js');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'test.22.11.2021@gmail.com',
+		pass: '18CNTNWNC'
+	}
+})
 
 const app = express();
 
@@ -26,11 +35,39 @@ app.get('/classes', async (req, res) => {
 	res.json(listClass);
 });
 
+function makeCode(length) {
+	var result = '';
+	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
+
 app.post('/classes', async (req, res) => {
 	const newClass = new Class(req.body);
+	newClass.code = makeCode(6);
 	await newClass.save();
 	res.status(202).json(newClass);
 });
+
+app.get('/sendInvite', async (req, res) => {
+	var mailOptions = {
+		from: 'ttdat.09.08.2000@gmail.com',
+		to: 'bobyba20@gmail.com',
+		subject: 'Invite to classroom',
+		text: 'You have been invited to join our classroom. If you don\'t join, Fuck you!!!'
+	}
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		}
+		else {
+			console.log("Email sent Success");
+		}
+	})
+})
 
 const host = '0.0.0.0';
 const port = process.env.PORT || 8080;
