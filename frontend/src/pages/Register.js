5e42294ebfb7d39cdf014276bcceea5bc6a5bf09
Nguyễn Thái Sơn <60878188+solatin@users.noreply.github.com>
2021-11-23 23:23:51 +0700

    Improve UI login and register

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,9 +1,11 @@
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { Helmet } from 'react-helmet';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router';
 import axiosClient from 'src/utils/axios';
+import LockIcon from '@mui/icons-material/Lock';
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
 	const navigate = useNavigate();
@@ -23,6 +25,8 @@ export const Register = () => {
 			</Helmet>
 			<Box sx={{ mx: 'auto', my: 'auto', width: '300px' }}>
 				<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexFlow: 'column', mt: '100px' }}>
+				<LockIcon color="primary" fontSize="large"/>
+          <Typography variant="h4" sx={{mb: 3}}>Register</Typography>
 					<TextField fullWidth label="Email" variant="outlined" {...register('email')} sx={{ mb: 2 }} />
 					<TextField fullWidth label="Password" variant="outlined" {...register('password')} sx={{ mb: 2 }} />
 					<FormControl sx={{ m: 1, minWidth: 80, width: '100%' }}>
@@ -35,6 +39,8 @@ export const Register = () => {
 					<Button variant="contained" onClick={handleSubmit(onSubmit)}>
 						Register
 					</Button>
+					or
+					<Link to="/login">Back to login page</Link>
 				</Box>
 			</Box>
 		</>
