92a9ac7ca5c1bba4c40eb07e42fc3dda880209b4
Quang <18120230@student.hcmus.edu.vn>
2021-11-24 16:34:34 +0700

    Add profile

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -2,26 +2,28 @@ import React from 'react';
 import { Navigate  } from 'react-router'
 import { useParams } from "react-router-dom";
 import authAxios from "src/utils/authAxios";
+import { useNavigate } from 'react-router-dom';
 
 import './Profile.css'
-
-const handleClick = () => {
-    // <Navigate to="/changePassword"/>
-}
 
 const Profile = () => {
     const [profileData, setProfileData] = React.useState({});
     const { id } = useParams();
+    const navigate = useNavigate();
 
-    // const fetch = async() => {
-    //     const rs = await authAxios.get(`/profile`);
-    //     console.log(rs);
-    //     setProfileData(rs);
-    // }
+    const fetch = async() => {
+        const rs = await authAxios.get(`/profile`);
+        console.log(rs);
+        setProfileData(rs);
+    }
 
-    // React.useEffect(() => {
-    //     fetch();
-    //   }, []);
+    React.useEffect(() => {
+        fetch();
+      }, []);
+
+    const handleClick = () => {
+        navigate('/changePassword');
+    }
 
     return(
         <>
@@ -29,12 +31,8 @@ const Profile = () => {
                 <h1>Profile</h1>
                 <img src="https://i.ibb.co/pdJfzx9/profile-picture.jpg" alt="profile-picture" border="0" />
                 <h4></h4>
-                <h4>Họ và tên: Trần Thanh Quang</h4> 
-                <h4>Email: queng@gmail.com</h4>
-                <h4>Mã số sinh viên: 18120230</h4> 
-                {/* {profileData.display_name}
-                {profileData.email}
-                {profileData.mssv} */}
+                <h4>Họ và tên: {profileData.display_name}</h4> 
+                <h4>Email: {profileData.email}</h4>
                 <button className="button" onClick={handleClick}>Change Password</button>
             </div>
         </>
