185de1626bae38f654b0d1e17f990335228b804e
ĐẠT TRẦN TUẤN <18120313@student.hcmus.edu.vn>
2022-01-18 16:31:08 +0700

    Add search function

--- frontend/src/containers/admin/ListClass.js
+++ frontend/src/containers/admin/ListClass.js
@@ -1,4 +1,3 @@
-import NotInterestedIcon from '@mui/icons-material/NotInterested';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import {
 	Button,
@@ -7,7 +6,6 @@ import {
 	DialogContent,
 	DialogTitle,
 	Divider,
-	FormHelperText,
 	IconButton,
 	Table,
 	TableBody,
@@ -21,12 +19,21 @@ import {
 import { Box } from '@mui/system';
 import { default as React, useEffect, useState } from 'react';
 import authAxios from 'src/utils/authAxios';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const ListClass = () => {
 	const [data, setData] = useState(null);
+
+	const [dataShow, setDataShow] = useState(null);
 	const fetch = async () => {
 		const rs = await authAxios.get('/admin/classes');
 		setData(
+			rs.map((user, index) => ({
+				...user,
+				id: index
+			}))
+		);
+		setDataShow(
 			rs.map((user, index) => ({
 				...user,
 				id: index
@@ -43,87 +50,106 @@ export const ListClass = () => {
 	useEffect(() => {
 		fetch();
 	}, []);
+	const searchData = (textSearch) => {
+		const filteredData = data.filter((classroom) => {
+			return classroom.class.name.toLowerCase().includes(textSearch.toLowerCase());
+		});
+		setDataShow(filteredData);
+	}
 	return (
-		<Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-			<Table sx={{ minWidth: 1000, width: 'auto' }} aria-label="caption table">
-				<TableHead>
-					<TableRow>
-						<TableCell sx={{ width:' 30%'}}>
-							<Typography sx={{ fontWeight: 600 }}>Name</Typography>
-						</TableCell>
-						<TableCell sx={{ width:' 20%'}}>
-							<Typography sx={{ fontWeight: 600 }}>Code</Typography>
-						</TableCell>
-						<TableCell sx={{ width:' 30%'}}>
-							<Typography sx={{ fontWeight: 600 }}>Owner</Typography>
-						</TableCell>
-						<TableCell align="center">
-							<Typography sx={{ fontWeight: 600 }}>NumberOfStudent</Typography>
-						</TableCell>
-						<TableCell align="center">
-							<Typography sx={{ fontWeight: 600 }}>Actions</Typography>
-						</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{data?.map((e) => (
-						<TableRow key={e._id}>
-							<TableCell component="th" scope="row">
-								{e.class.name}
+		<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'column', marginTop: 5 }}>
+			<TextField
+				id="outlined-basic"
+				label="Search"
+				variant="outlined"
+				sx={{ minWidth: 1000, width: 'auto' }}
+				onChange={(e) => searchData(e.target.value)}
+				InputProps={{
+					startAdornment: (
+						<SearchIcon />
+					),
+				}} />
+			<Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+				<Table sx={{ minWidth: 1000, width: 'auto' }} aria-label="caption table">
+					<TableHead>
+						<TableRow>
+							<TableCell sx={{ width: ' 30%' }}>
+								<Typography sx={{ fontWeight: 600 }}>Name</Typography>
 							</TableCell>
-							<TableCell sx={{ textTransform: 'capitalize' }}>{e.class.code}</TableCell>
-							<TableCell sx={{ textTransform: 'capitalize' }}>{e.owner?.display_name}</TableCell>
-							<TableCell sx={{ textTransform: 'capitalize' }} align="center">{e.numberOfStudent}</TableCell>
-							<TableCell>
-								<Tooltip title="Details">
-									<IconButton onClick={() => seeDetails(e)}>
-										<RemoveRedEyeIcon sx={{ color: 'green' }} />
-									</IconButton>
-								</Tooltip>
+							<TableCell sx={{ width: ' 20%' }}>
+								<Typography sx={{ fontWeight: 600 }}>Code</Typography>
+							</TableCell>
+							<TableCell sx={{ width: ' 30%' }}>
+								<Typography sx={{ fontWeight: 600 }}>Owner</Typography>
+							</TableCell>
+							<TableCell align="center">
+								<Typography sx={{ fontWeight: 600 }}>NumberOfStudent</Typography>
+							</TableCell>
+							<TableCell align="center">
+								<Typography sx={{ fontWeight: 600 }}>Actions</Typography>
 							</TableCell>
 						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-			<Dialog open={!!classDetails} onClose={closeSeeDetails} fullWidth maxWidth="sm">
-				<DialogTitle id="alert-dialog-title">
-					<Typography variant="h5" textAlign="center">
-						Class detail
-					</Typography>
-				</DialogTitle>
-				<Divider />
-				<DialogContent>
-					<Box>
-						<Box sx={{ display: 'inline-block', width: 200 }}>
-							<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>Name</Typography>
+					</TableHead>
+					<TableBody>
+						{dataShow?.map((e) => (
+							<TableRow key={e._id}>
+								<TableCell component="th" scope="row">
+									{e.class.name}
+								</TableCell>
+								<TableCell sx={{ textTransform: 'capitalize' }}>{e.class.code}</TableCell>
+								<TableCell sx={{ textTransform: 'capitalize' }}>{e.owner?.display_name}</TableCell>
+								<TableCell sx={{ textTransform: 'capitalize' }} align="center">{e.numberOfStudent}</TableCell>
+								<TableCell>
+									<Tooltip title="Details">
+										<IconButton onClick={() => seeDetails(e)}>
+											<RemoveRedEyeIcon sx={{ color: 'green' }} />
+										</IconButton>
+									</Tooltip>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+				<Dialog open={!!classDetails} onClose={closeSeeDetails} fullWidth maxWidth="sm">
+					<DialogTitle id="alert-dialog-title">
+						<Typography variant="h5" textAlign="center">
+							Class detail
+						</Typography>
+					</DialogTitle>
+					<Divider />
+					<DialogContent>
+						<Box>
+							<Box sx={{ display: 'inline-block', width: 200 }}>
+								<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>Name</Typography>
+							</Box>
+							{classDetails?.class?.name}
 						</Box>
-						{classDetails?.class?.name}
-					</Box>
-					<Box>
-						<Box sx={{ display: 'inline-block', width: 200 }}>
-							<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>Code</Typography>
+						<Box>
+							<Box sx={{ display: 'inline-block', width: 200 }}>
+								<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>Code</Typography>
+							</Box>
+							{classDetails?.class?.code}
 						</Box>
-						{classDetails?.class?.code}
-					</Box>
-					<Box>
-						<Box sx={{ display: 'inline-block', width: 200 }}>
-							<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>Owner</Typography>
+						<Box>
+							<Box sx={{ display: 'inline-block', width: 200 }}>
+								<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>Owner</Typography>
+							</Box>
+							{classDetails?.owner?.display_name}
 						</Box>
-						{classDetails?.owner?.display_name}
-					</Box>
-					<Box>
-						<Box sx={{ display: 'inline-block', width: 200 }}>
-							<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>Number of student</Typography>
+						<Box>
+							<Box sx={{ display: 'inline-block', width: 200 }}>
+								<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>Number of student</Typography>
+							</Box>
+							{classDetails?.numberOfStudent}
 						</Box>
-						{classDetails?.numberOfStudent}
-					</Box>
-				</DialogContent>
-				<DialogActions sx={{ p: 3, pt: 0 }}>
-					<Button onClick={closeSeeDetails} autoFocus variant="outlined">
-						Cancel
-					</Button>
-				</DialogActions>
-			</Dialog>
+					</DialogContent>
+					<DialogActions sx={{ p: 3, pt: 0 }}>
+						<Button onClick={closeSeeDetails} autoFocus variant="outlined">
+							Cancel
+						</Button>
+					</DialogActions>
+				</Dialog>
+			</Box>
 		</Box>
 	);
 };
--- frontend/src/containers/admin/ListUser.js
+++ frontend/src/containers/admin/ListUser.js
@@ -7,7 +7,6 @@ import {
 	DialogContent,
 	DialogTitle,
 	Divider,
-	FormHelperText,
 	IconButton,
 	Table,
 	TableBody,
@@ -21,12 +20,21 @@ import {
 import { Box } from '@mui/system';
 import { default as React, useEffect, useState } from 'react';
 import authAxios from 'src/utils/authAxios';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const ListUser = () => {
 	const [data, setData] = useState(null);
+	const [dataShow, setDataShow] = useState(null);
 	const fetch = async () => {
 		const rs = await authAxios.get('/admin/users');
 		setData(
+			rs.map((user, index) => ({
+				...user,
+				id: index,
+				actions: ''
+			}))
+		);
+		setDataShow(
 			rs.map((user, index) => ({
 				...user,
 				id: index,
@@ -52,83 +60,106 @@ export const ListUser = () => {
 	useEffect(() => {
 		fetch();
 	}, []);
+	const searchData = (textSearch) => {
+		const filteredData = data.filter((account) => {
+			return account.display_name.toLowerCase().includes(textSearch.toLowerCase());
+		});
+		setDataShow(filteredData);
+	}
 	return (
-		<Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-			<Table sx={{ minWidth: 600, width: 'auto' }} aria-label="caption table">
-				<TableHead>
-					<TableRow>
-						<TableCell>
-							<Typography sx={{ fontWeight: 600 }}>Email</Typography>
-						</TableCell>
-						<TableCell>
-							<Typography sx={{ fontWeight: 600 }}>Role</Typography>
-						</TableCell>
-						<TableCell>
-							<Typography sx={{ fontWeight: 600 }}>Actions</Typography>
-						</TableCell>
-						<TableCell align="center">
-							<Typography sx={{ fontWeight: 600 }}>Ban/UnBan</Typography>
-						</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{data?.map((user) => (
-						<TableRow key={user._id}>
-							<TableCell component="th" scope="row">
-								{user.email}
-							</TableCell>
-							<TableCell sx={{ textTransform: 'capitalize' }}>{user.role}</TableCell>
+		<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'column', marginTop: 5 }}>
+			<TextField
+				id="outlined-basic"
+				label="Search"
+				variant="outlined"
+				sx={{ minWidth: 800, width: 'auto' }}
+				onChange={(e) => searchData(e.target.value)}
+				InputProps={{
+					startAdornment: (
+						<SearchIcon />
+					),
+				}} />
+			<Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+				<Table sx={{ minWidth: 800, width: 'auto' }} aria-label="caption table">
+					<TableHead>
+						<TableRow>
 							<TableCell>
-								<Tooltip title="Details">
-									<IconButton onClick={() => seeDetails(user)}>
-										<RemoveRedEyeIcon sx={{ color: 'green' }} />
-									</IconButton>
-								</Tooltip>
+								<Typography sx={{ fontWeight: 600 }}>Email</Typography>
+							</TableCell>
+							<TableCell>
+								<Typography sx={{ fontWeight: 600 }}>Role</Typography>
+							</TableCell>
+							<TableCell>
+								<Typography sx={{ fontWeight: 600 }}>Name</Typography>
+							</TableCell>
+							<TableCell>
+								<Typography sx={{ fontWeight: 600 }}>Actions</Typography>
 							</TableCell>
 							<TableCell align="center">
-								{user.status === 'banned' ? (
-									<Tooltip title="Unban">
-										<IconButton onClick={() => unBanAccount(user._id)}>
-											<NotInterestedIcon sx={{ color: 'green' }} />
-										</IconButton>
-									</Tooltip>
-								) : (
-									<Tooltip title="Ban">
-										<IconButton onClick={() => banAccount(user._id)}>
-											<NotInterestedIcon sx={{ color: 'red' }} />
-										</IconButton>
-									</Tooltip>
-								)}
+								<Typography sx={{ fontWeight: 600 }}>Ban/UnBan</Typography>
 							</TableCell>
 						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-			<Dialog open={!!userDetails} onClose={closeSeeDetails} fullWidth maxWidth="sm">
-				<DialogTitle id="alert-dialog-title">
-					<Typography variant="h5" textAlign="center">
-						User detail
-					</Typography>
-				</DialogTitle>
-				<Divider />
-				<DialogContent>
-					{['email', 'display_name', 'role'].map((field) => (
-            <Box>
-						<Box sx={{ display: 'inline-block', width: 200}}>
-							<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>
-								{field === "display_name" ? "Display Name" : field}
-							</Typography>
-						</Box>
-							{userDetails && userDetails[field]}
-              </Box>
-					))}
-				</DialogContent>
-				<DialogActions sx={{ p: 3, pt: 0 }}>
-					<Button onClick={closeSeeDetails} autoFocus variant="outlined">
-						Cancel
-					</Button>
-				</DialogActions>
-			</Dialog>
+					</TableHead>
+					<TableBody>
+						{dataShow?.map((user) => (
+							<TableRow key={user._id}>
+								<TableCell component="th" scope="row">
+									{user.email}
+								</TableCell>
+								<TableCell sx={{ textTransform: 'capitalize' }}>{user.role}</TableCell>
+								<TableCell sx={{ textTransform: 'capitalize' }}>{user.display_name}</TableCell>
+								<TableCell>
+									<Tooltip title="Details">
+										<IconButton onClick={() => seeDetails(user)}>
+											<RemoveRedEyeIcon sx={{ color: 'green' }} />
+										</IconButton>
+									</Tooltip>
+								</TableCell>
+								<TableCell align="center">
+									{user.status === 'banned' ? (
+										<Tooltip title="Unban">
+											<IconButton onClick={() => unBanAccount(user._id)}>
+												<NotInterestedIcon sx={{ color: 'green' }} />
+											</IconButton>
+										</Tooltip>
+									) : (
+										<Tooltip title="Ban">
+											<IconButton onClick={() => banAccount(user._id)}>
+												<NotInterestedIcon sx={{ color: 'red' }} />
+											</IconButton>
+										</Tooltip>
+									)}
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+				<Dialog open={!!userDetails} onClose={closeSeeDetails} fullWidth maxWidth="sm">
+					<DialogTitle id="alert-dialog-title">
+						<Typography variant="h5" textAlign="center">
+							User detail
+						</Typography>
+					</DialogTitle>
+					<Divider />
+					<DialogContent>
+						{['email', 'display_name', 'role'].map((field) => (
+							<Box>
+								<Box sx={{ display: 'inline-block', width: 200 }}>
+									<Typography variant="h6" sx={{ textTransform: 'capitalize', mr: 2 }}>
+										{field === "display_name" ? "Display Name" : field}
+									</Typography>
+								</Box>
+								{userDetails && userDetails[field]}
+							</Box>
+						))}
+					</DialogContent>
+					<DialogActions sx={{ p: 3, pt: 0 }}>
+						<Button onClick={closeSeeDetails} autoFocus variant="outlined">
+							Cancel
+						</Button>
+					</DialogActions>
+				</Dialog>
+			</Box>
 		</Box>
 	);
 };
